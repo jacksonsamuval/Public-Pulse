c83f45b5e56cf96154a9f498620ef51cd4c4ae5d
Jackson <jacksonsamuelm10@gmail.com>
2025-09-20 16:16:24 +0530

    Initial Commit

---
+++ Backend/src/main/java/com/finalyear/publicpulse/PublicpulseApplication.java
@@ -0,0 +1,13 @@
+package com.finalyear.publicpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicpulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicpulseApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/finalyear/publicpulse/PublicpulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalyear.publicpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicpulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
